187051e2fd653b252a28b18be2c43e757195515f
avikal <avikalprakash@gmail.com>
2020-08-19 00:07:45 +0530

    Splash screen

---
+++ app/src/main/java/com/project/assignment/Utils/AppConstant.java
@@ -0,0 +1,5 @@
+package com.project.assignment.Utils;
+
+public class AppConstant {
+    public static int SPLASH_TIME_OUT = 5000;
+}
---
+++ app/src/main/java/com/project/assignment/app_initialization/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.project.assignment.app_initialization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.project.assignment.MainActivity;
+import com.project.assignment.R;
+import com.project.assignment.Utils.AppConstant;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start the app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, AppConstant.SPLASH_TIME_OUT);
+    }
+}
